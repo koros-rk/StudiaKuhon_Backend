c02ddecada5cdc95e2ab973ae31bec16596e7a7d
koros <korosov.j@gmail.com>
2023-03-21 23:55:19 +0200

    final fixes

--- disign_shop/Filters/ProductFilter.py
+++ disign_shop/Filters/ProductFilter.py
@@ -9,8 +9,9 @@ class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
 class ProductFilter(filters.FilterSet):
     styles = CharFilterInFilter(field_name="styles__title", lookup_expr="in")
     materials = CharFilterInFilter(field_name="materials__title", lookup_expr="in")
-    colours = CharFilterInFilter(field_name="colours__title", lookup_expr="in")
+    colors = CharFilterInFilter(field_name="colors__title", lookup_expr="in")
+    handle = CharFilterInFilter(field_name="handle__title", lookup_expr="in")
 
     class Meta:
         model = Product
-        fields = ['styles', 'materials', 'colours']
+        fields = ['styles', 'materials', 'colors', 'handle']
